9a058f273357c0478578a4be14997e860612c84a
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2025-09-30 20:36:49 -0300

    feat: criando estrutura inicial da api

---
+++ src/main/java/br/com/oaksystem/oaksystem/OakSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.oaksystem.oaksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OakSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OakSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/oaksystem/oaksystem/OakSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oaksystem.oaksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OakSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
